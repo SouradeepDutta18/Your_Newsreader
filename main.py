598002c1b2ccdebd4128e30d5eb0a2d73976b228
Souradeep Dutta <souradeepdutta2001@gmail.com>
2021-01-09 19:55:21 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from win32com.client import Dispatch
+import json
+with open("B.txt","r") as f:
+    key = f.read()
+
+def say(str):
+ speak = Dispatch("SAPI.SpVoice")
+ speak.Speak(str)
+
+news = requests.get(f"http://newsapi.org/v2/top-headlines?country=in&apiKey={key}")  #putyour api key in the curly braces.
+news = news.json()
+articles = news["articles"]
+i = 0
+for article in articles:
+ say(article["title"])
+ i+=1
+
